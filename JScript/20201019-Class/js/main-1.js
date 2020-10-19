afa8b8d0a70ef9f71f8b95085a4833b79fc8e80b
Phil[go] <philipe.ng@protonmail.com>
2020-10-19 14:05:17 -0400

    Started JavaScript Classes

---
+++ JScript/20201019-Class/js/main-1.js
@@ -0,0 +1,10 @@
+// ======================= RULES and SYNTAX =====================
+// Case sensitive language -> myName != MyName
+// no sensitive to white space -> ignores spaces, tabs, etc.MyName
+// close statementes with a ;
+// it is an INTERPRETER language
+// 
+
+var myH1 = document.getElementsByTagName("h1")[0].textContent;
+
+alert(myH1);
